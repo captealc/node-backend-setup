f0fe2f5951b2cdc3a596dfc3039c4cac31a7847e
captealc <52834706+captealc@users.noreply.github.com>
2019-07-12 22:11:19 +0200

    NPM commands

--- src/logger.js
+++ src/logger.js
@@ -3,10 +3,9 @@ import * as winston from 'winston'
 const logger = winston.createLogger({
   level: 'debug',
   format: winston.format.combine(
-    winston.format.colorize({ all: true }),
     winston.format.timestamp(),
     winston.format.printf((info) => {
-      return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.stack || info.message}`
+      return `[${info.timestamp}][${info.level.toUpperCase()}]: ${info.stack || info.message}`
     })
   ),
   transports: [
