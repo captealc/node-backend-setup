22111b555d605a5d2abf897114a4d860054247f4
captealc <52834706+captealc@users.noreply.github.com>
2019-07-12 20:53:00 +0200

    Make async work

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,10 @@
 import {logger} from './logger'
+import {promisify} from "util"
+import * as fs from 'fs'
 
-const sayHello = () => {
+const sayHello = async () => {
+  let writeFileAsync = promisify(fs.writeFile)
+  await writeFileAsync("/tmp/test", "elllo")
   logger.info("Hello")
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import {sayHello} from './hello'
 import {logger} from './logger'
 
-try {
-  sayHello()
-} catch (e) {
-  logger.error(e)
-}
+(async () => {
+  try {
+    await sayHello()
+  } catch (e) {
+    logger.error(e)
+  }
+})()
