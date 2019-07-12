349ac9a687342194dcb9a853ee8a7ded1eaa7276
captealc <52834706+captealc@users.noreply.github.com>
2019-07-12 22:41:00 +0200

    test linters

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import { sayHello } from "./hello";
-import { logger } from "./logger";
-(async () => {
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import { sayHello } from './hello'
+import { logger } from './logger'
+
+;(async () => {
   try {
-    await sayHello();
+    await sayHello()
   } catch (e) {
-    logger.error(e);
+    logger.error(e)
   }
-})();
+})()
