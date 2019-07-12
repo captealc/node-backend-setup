8c4eeb18e83d8be0517a669ab177cd990b283c6c
captealc <52834706+captealc@users.noreply.github.com>
2019-07-12 23:21:27 +0200

    Fix line numbers in logs

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,9 @@
 import * as winston from 'winston'
 
-const logger = winston.createLogger({
+const path = require('path')
+const PROJECT_ROOT = path.join(__dirname, '..')
+
+const winstonLogger = winston.createLogger({
   level: 'debug',
   format: winston.format.combine(
     winston.format.timestamp(),
@@ -11,5 +14,74 @@ const logger = winston.createLogger({
   ),
   transports: [new winston.transports.Console({})]
 })
+
+winstonLogger.stream = {
+  write: function (message) {
+    winstonLogger.info(message)
+  }
+}
+
+const debug = function () {
+  winstonLogger.debug.apply(winstonLogger, formatLogArguments(arguments))
+}
+
+const info = function () {
+  winstonLogger.info.apply(winstonLogger, formatLogArguments(arguments))
+}
+
+const warn = function () {
+  winstonLogger.warn.apply(winstonLogger, formatLogArguments(arguments))
+}
+
+const error = function () {
+  winstonLogger.error.apply(winstonLogger, formatLogArguments(arguments))
+}
+
+module.exports.stream = winstonLogger.stream
+
+function formatLogArguments (args) {
+  args = Array.prototype.slice.call(args)
+
+  var stackInfo = getStackInfo(1)
+
+  if (stackInfo) {
+    const calleeStr = '(' + stackInfo.relativePath + ':' + stackInfo.line + ')'
+
+    if (typeof args[0] === 'string') {
+      args[0] = calleeStr + ' ' + args[0]
+    } else {
+      args.unshift(calleeStr)
+    }
+  }
+
+  return args
+}
+
+function getStackInfo (stackIndex) {
+  const stacklist = new Error().stack.split('\n').slice(3)
+  const stackReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/gi
+  const stackReg2 = /at\s+()(.*):(\d*):(\d*)/gi
+
+  const s = stacklist[stackIndex] || stacklist[0]
+  const sp = stackReg.exec(s) || stackReg2.exec(s)
+
+  if (sp && sp.length === 5) {
+    return {
+      method: sp[1],
+      relativePath: path.relative(PROJECT_ROOT, sp[2]),
+      line: sp[3],
+      pos: sp[4],
+      file: path.basename(sp[2]),
+      stack: stacklist.join('\n')
+    }
+  }
+}
+
+const logger = {
+  error,
+  info,
+  debug,
+  warn
+}
 
 export { logger }
